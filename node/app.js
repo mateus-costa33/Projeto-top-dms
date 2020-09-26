7022ad2778a1889eaa4e39619abc96d8c4e1297a
Nicoly-Almeida <nicolyejady@gmail.com>
2020-09-26 16:52:29 -0300

    altas coisas

--- node/app.js
+++ node/app.js
@@ -1,7 +1,7 @@
 const api = 'http://127.0.0.1:3000';
 
 async function create(livro) {
-  const res = await fetch(`${api}/livro`, {
+  const res = await fetch(`${api}/livros`, {
     method: 'post',
     body: JSON.stringify(livro),
     headers: {
@@ -11,3 +11,41 @@ async function create(livro) {
 
   return await res.json();
 }
+async function readAll() {
+  const res = await fetch(`${api}/livros`);
+
+  return await res.json();
+}
+
+async function readByTitulo(titulo) {
+  const res = await fetch(`${api}/livros/${titulo}`);
+
+  return await res.json();
+}
+async function readByAutor(autor) {
+  const res = await fetch(`${api}/livros?autor=${autor}`);
+
+  return await res.json();
+}
+async function update(id, livro) {
+  const res = await fetch(`${api}/livros/${id}`,
+  {
+    method: 'put',
+    body: JSON.stringify(livro),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  }
+  );
+
+  return await res.json();
+}
+async function remove(id) {
+  const res = await fetch(`${api}/livros/${id}`,
+  {
+    method:'delete',
+  });
+
+}
+
+export { create,readAll,readByAutor,readByTitulo, update, remove };
--- node/index.js
+++ node/index.js
@@ -1,1 +1,50 @@
-let eu = top;
+import {create,readAll,readByAutor,readByTitulo, update, remove}  from './app.js';
+
+
+const livro = {
+    id : 1,
+    titulo : "Era uma vez",
+    autor : "Nick & Ray",
+};
+
+document.querySelector('#create').addEventListener('click', async () => {
+    const livros = await create();
+
+    console.log(livros);
+});
+
+document.querySelector('#buscar-all').addEventListener('click', async () => {
+    const livros = await readAll();
+
+    console.log(livros);
+});
+
+document.querySelector('#buscar-autor').addEventListener('click', async () => {
+    const livros = await readByAutor();
+
+    console.log(livros);
+});
+
+document.querySelector('#buscar-titulo').addEventListener('click', async () => {
+    const livros = await readByTitulo();
+
+    console.log(livros);
+});
+
+document.querySelector('#buscar-autor').addEventListener('click', async () => {
+    const livros = await readByAutor();
+
+    console.log(livros);
+});
+
+document.querySelector('#atualizar').addEventListener('click', async () => {
+    const livros = await update();
+
+    console.log(livros);
+});
+
+document.querySelector('#remover').addEventListener('click', async () => {
+    const livros = await remove();
+
+    console.log(livros);
+});
